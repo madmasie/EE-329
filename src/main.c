fb89ee46975b3206628f101cb2e4b27b60246f5f
maddiemasie <maddiemasiello@gmail.com>
2025-04-04 12:56:17 -0700

    comments

--- src/main.c
+++ src/main.c
@@ -2,25 +2,36 @@
 a binary count to 15 using LEDs and an instruction execution timer. */
 
 /* Instructions:
-(a) LED Counter - Connect 4 LEDs to GPIO pins PC0, PC1, PC2, and PC3. (series LED and 560 Ω R).
-Use data on the LED datasheet to calculate the expected current from a VOH output then measure
-the actual GPIO output voltage, LED voltage, and LED ON current. Write code in main() that repeatedly 
-counts from 0 to 15 and displays the binary count on the 4 LEDs. Use a calibrated software delay 
-(empty ‘for’ loop) between counts to make the LED blinks visible. Attach an oscilloscope to the LSb LED 
-to measure the for loop software delay for a few different delay settings and estimate the delay per for loop iteration. 
+(a) LED Counter:
+    1. Connect 4 LEDs to GPIO pins PC0, PC1, PC2, and PC3. (series LED and 560 Ω R).
+    Use data on the LED datasheet to calculate the expected current from a VOH output then measure
+    the actual GPIO output voltage, LED voltage, and LED ON current. 
 
-(b) Measure Execution Timing
-Further leverage ExecutionTime.c, specifically the TestFunction() function, to add instruction execution 
-timing to your main() routine. Code it so that the execution timing starts after 3 iterations through 
-the count to 15 on the LEDs then repeats continuously.Set up an oscilloscope or logic analyzer to capture
-the pulses on PC0 and PC1 to measure the durations of (a) the call to TestFunction() and (b) the 
-instruction inside TestFunction(). Notice that PC0 is toggled before and after the call to TestFunction()
-and PC1 is toggled before and after the arithmetic instruction inside the function. Change var_type and 
-TestFunction(num) to test the combinations listed in Table A1(b). Run the code iteratively with the prescribed
-settings to complete the table. To assess the delay entering the function, measure the interval from PC0
-going high to PC1 going high. This ‘function call latency’ will not vary for different arithmetic
-instructions inside the function, so use the simple ‘test_var = num’ case to fill row 1 of the table. 
-*/
+    2. Write code in main() that repeatedly 
+    counts from 0 to 15 and displays the binary count on the 4 LEDs. Use a calibrated software delay 
+    (empty ‘for’ loop) between counts to make the LED blinks visible. Attach an oscilloscope to the LSb LED 
+    to measure the for loop software delay for a few different delay settings and estimate the delay per for loop iteration. 
+
+(b) Measure Execution Timing:
+    1.Further leverage ExecutionTime.c, specifically the TestFunction() function, to add instruction execution 
+    timing to your main() routine. Code it so that the execution timing starts after 3 iterations through 
+    the count to 15 on the LEDs then repeats continuously. 
+
+    2. Set up an oscilloscope or logic analyzer to capture
+    the pulses on PC0 and PC1 to measure the durations of (a) the call to TestFunction() and (b) the 
+    instruction inside TestFunction(). Notice that PC0 is toggled before and after the call to TestFunction()
+    and PC1 is toggled before and after the arithmetic instruction inside the function. 
+
+    3. Change var_type and TestFunction(num) to test the combinations listed in Table A1(b). 
+
+    4. Run the code iteratively with the prescribed
+    settings to complete the table. To assess the delay entering the function, measure the interval from PC0
+    going high to PC1 going high. This ‘function call latency’ will not vary for different arithmetic
+    instructions inside the function, so use the simple ‘test_var = num’ case to fill row 1 of the table. 
+
+    Hints: Modify ExecutionTime.c to call TestFunction() repeatedly to ease oscilloscope analysis. Consider 
+    using a typedef declaration to facilitate quick switching of var_type between tests.
+    */
 
 
 // file ExecutionTime.c sample code for A1 
