5e653e0eebff8209a7bb95c8ad24fb356dd3a699
Your Name <you@example.com>
2025-04-11 19:40:02 -0700

    changed main.h file

--- A2/include/main.h
+++ A2/include/main.h
@@ -1,24 +1,46 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+/*******************************************************************************
+ * EE 329 A2 KEYPAD INTERFACE 
+ *******************************************************************************
+ * @file           : main.c
+ * @brief          : keypad configuration and debounced detection of keypresses
+ * project         : EE 329 S'23 Assignment A2
+ * authors         : Maddie Masiello
+ * version         : 1
+ * date            : 230413
+ * compiler        : STM32CubeIDE v.1.12.0 Build: 14980_20230301_1550 (UTC) 
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2023 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * KEYPAD PLAN :
+ * set columns as outputs, rows as inputs w pulldowns
+ * loop:
+ * drive all columns HI read all rows
+ * if any row N is HI
+ *    set all columns LO
+ *    drive each column M HI alone
+ *    read row N until HI 🡪 pressed key loc’n = N, M
+ * key value = 3N+M+1 for 1..9,special case for *,0,#
+ *******************************************************************************
+ * KEYPAD WIRING 4 ROWS 3 COLS (pinout NUCLEO-L4A6ZG = L496ZG)
+ *      peripheral – Nucleo I/O
+ * keypad 1  COL 2 - PF13= CN10- 2 - OUT
+ * keypad 2  ROW 1 - PF0 = CN9 -21 - IN, PD ON
+ * keypad 3  COL 1 - PF12= CN7 -20 - OUT
+ * keypad 4  ROW 4 - PF3 = CN8 -14 - IN, PD ON
+ * keypad 5  COL 3 - PF14= CN10- 8 - OUT
+ * keypad 6  ROW 3 - PF2 = CN9 -17 - IN, PD ON
+ * keypad 7  ROW 2 - PF1 = CN9 -19 - IN, PD ON
+ *******************************************************************************
+ * REVISION HISTORY
+* 230413 - Masiello - initial version
+ *******************************************************************************
+ * TODO
+ * 
+ *******************************************************************************
+*/
 
-/* Define to prevent recursive inclusion -------------------------------------*/
+
 #ifndef __MAIN_H
 #define __MAIN_H
 
@@ -29,41 +51,11 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
 /* Exported functions prototypes ---------------------------------------------*/
+void SystemClock_Config(void);
 void Error_Handler(void);
-
-/* USER CODE BEGIN EFP */
-
-/* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* __MAIN_H */
+#endif 
