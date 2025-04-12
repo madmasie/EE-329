cda0538d23c1fc7105e420f191ffa37bb527b981
Your Name <you@example.com>
2025-04-11 20:13:06 -0700

    changed keypad.h

--- A2/include/keypad.h
+++ A2/include/keypad.h
@@ -21,4 +21,37 @@
  * TODO
  * 
  *******************************************************************************
+*/
+   
+#ifndef INC_KEYPAD_H_
+#define INC_KEYPAD_H_
 
+#include "stm32l4xx_hal.h"
+
+// ------------------------ KEYPAD Defines ------------------------
+#define COL_PORT GPIOF
+#define COL_PINS (GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14)
+
+#define ROW_PORT GPIOF
+#define ROW_PINS (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3)
+
+// Settle time for GPIO read delay loop
+#define SETTLE 1900  // ~20ms debounce (5.5us * 1900)
+
+// Bit 0 for shifting logic
+#define BIT0_COL GPIO_PIN_12
+#define BIT0_ROW GPIO_PIN_0
+
+#define NUM_COLS 3
+#define NUM_ROWS 4
+
+#define NO_KEYPRESS 0x0
+#define KEY_ZERO 11
+#define CODE_ZERO 0xF
+
+// ------------------------ Function Prototypes ------------------------
+void Keypad_Config(void);
+int Keypad_IsAnyKeyPressed(void);
+int Keypad_WhichKeyIsPressed(void);
+
+#endif /* INC_KEYPAD_H_ */
